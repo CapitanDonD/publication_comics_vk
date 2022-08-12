72c12f7e8d2fa86aa76b67d7308b185883fb4f82
CapitanDonD <79704733+CapitanDonD@users.noreply.github.com>
2022-08-12 17:57:48 +0700

    commit for error 121

--- main.py
+++ main.py
@@ -1,26 +1,71 @@
+import os
+
 import requests
+from dotenv import load_dotenv
+from pprint import pprint
 
 
-def download_picture(filepath, url):
+def download_xkcd_picture():
+    url = 'https://xkcd.com/353/info.0.json'
     response = requests.get(url)
     image_url = response.json()['img']
     comment = response.json()['alt']
     image_response = requests.get(image_url)
 
-
     response.raise_for_status()
 
-    with open(filepath, "wb") as file:
+    with open('image.jpg', "wb") as file:
         file.write(image_response.content)
 
     return comment
 
+def get_server_inf(group_id, token):
+    url = 'https://api.vk.com/method/photos.getWallUploadServer'
+    params = {
+        'group_id': group_id,
+        'access_token': token,
+        'v': '5.131'
+    }
+    response = requests.get(url, params=params)
+
+    return response.json()['response']
+
+def upload_server_picture(token):
+    with open('image.jpg', 'rb') as file:
+        url = get_server_inf(214371524, token)['upload_url']
+        files = {
+            'photo': file
+        }
+        response = requests.post(url, files=files)
+    photo = response.json()['photo']
+    server = response.json()['server']
+    hash = response.json()['hash']
+
+    return photo, server, hash
+
+def save_wall_photo(token):
+    photo, server, hash = upload_server_picture(token)
+    url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    photo = upload_server_picture(token)[0]
+    server = upload_server_picture(token)[1]
+    hash = upload_server_picture(token)[2]
+    params = {
+        'group_id': 214371524,
+        'server': server,
+        'photo': photo,
+        'hash': hash,
+        'access_token': token,
+        'v': '5.131'
+    }
+    response = requests.post(url, params=params)
+
+    return response.json()
+
 
 if __name__ == "__main__":
-    file_name = 'image.png'
-    url = 'https://xkcd.com/353/info.0.json'
+    load_dotenv()
 
-    print(download_picture(
-        file_name,
-        url,
-    ))
+    token = os.getenv('ACCESS_TOKEN')
+
+    download_xkcd_picture()
+    print(save_wall_photo(token))
